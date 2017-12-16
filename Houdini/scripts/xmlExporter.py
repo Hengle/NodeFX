950a9a4e18070fab9db2de646917c605019771ff
Chris Judkins <mail@chrisjudkins.com>
2017-12-16 09:39:11 -0500

    Refactored XMLExporter, looking much nicer!

--- Houdini/scripts/xmlExporter.py
+++ Houdini/scripts/xmlExporter.py
@@ -1,38 +1,37 @@
 import xml.etree.cElementTree as ET
 import hou
 
-node = hou.pwd().parent().type().definition()
 geo = hou.pwd().geometry()
 numEmitters = hou.pwd().geometry().intAttribValue("numEmitters")
-
 root = ET.Element("root")
+root.set("emitterCount", str(numEmitters))
 
+#   Loop over emitters
 for i in range(0, numEmitters):
     emitterName = "emitter" + str(i)
-    currentEmitter = ET.SubElement(root, emitterName)
+    emitterElement = ET.SubElement(root, emitterName)
 
+    #   Loop over attributes in emitter
     for attribute in geo.pointAttribs():
-        key = attribute.name()
-        currentAttribute = ET.SubElement(currentEmitter, "attribute")
-        if attribute.dataType() == hou.attribData.Int:
-            currentAttribute.set("type", "int")
-            for index, attribValue in enumerate(geo.iterPoints()[i].intListAttribValue(attribute.name())):
-                attributeElement = ET.SubElement(currentAttribute, "element")
-                attributeElement.set("index", str(index))
-                attributeElement.set("value", str(attribValue))
-        if attribute.dataType() == hou.attribData.Float:
-            currentAttribute.set("type", "float")
-            for index, attribValue in enumerate(geo.iterPoints()[i].floatListAttribValue(attribute.name())):
-                attributeElement = ET.SubElement(currentAttribute, "element")
-                attributeElement.set("index", str(index))
-                attributeElement.set("value", str(attribValue))
-        if attribute.dataType() == hou.attribData.String:
-            currentAttribute.set("type", "string")
-            for index, attribValue in enumerate(geo.iterPoints()[i].stringListAttribValue(attribute.name())):
-                attributeElement = ET.SubElement(currentAttribute, "element")
-                attributeElement.set("index", str(index))
-                attributeElement.set("value", str(attribValue))
-        currentAttribute.set("name", key)
+        attributeElement = ET.SubElement(emitterElement, "attribute")
+        attributeElement.text = attribute.name()
 
-tree = ET.ElementTree(root)
-tree.write("filename.xml")
+        if attribute.dataType() == hou.attribData.Int:
+            attributeElement.set("type", "int")
+            values = geo.iterPoints()[i].intListAttribValue(attribute.name())
+
+        if attribute.dataType() == hou.attribData.Float:
+            attributeElement.set("type", "float")
+            values = geo.iterPoints()[i].floatListAttribValue(attribute.name())
+
+        if attribute.dataType() == hou.attribData.String:
+            attributeElement.set("type", "string")
+            values = geo.iterPoints()[i].stringListAttribValue(attribute.name())
+
+        #   Loop over values in attribute
+        for index, value in enumerate(values):
+            valueElement = ET.SubElement(attributeElement, "value")
+            valueElement.set("index", str(index))
+            valueElement.text = str(value)
+
+ET.ElementTree(root).write("filename.xml")
