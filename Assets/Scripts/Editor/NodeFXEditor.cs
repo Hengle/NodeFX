ba4d97a27e228cddb82095463983ad4a20352b76
Chris Judkins <mail@chrisjudkins.com>
2017-12-18 17:05:11 -0500

    Added separate effect inspector class

---
+++ Assets/Scripts/Editor/NodeFXEditor.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using NodeFX;
+using System;
+using System.IO;
+
+[CustomEditor(typeof(NodeFXEffect))]
+public class NodeFXEditor : Editor {
+
+	NodeFXEffect targetEffect;
+
+	private FileSystemWatcher _fileSystemWatcher;
+	private GUIStyle headerStyle = new GUIStyle();
+
+	void OnEnable() {
+		targetEffect = (NodeFXEffect)target;
+		GenerateStyles();
+        targetEffect._isDirty = true;
+	}
+
+	public override void OnInspectorGUI()
+    {
+		GUIDrawHeader();
+
+        targetEffect.effectDefinition = (TextAsset)EditorGUILayout.ObjectField("Effect Definition", targetEffect.effectDefinition, typeof(TextAsset),false);
+		
+        GUIDrawTopShelfButtons();
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        GUIDrawRefreshToggles();
+
+        CheckForUpdates();
+    }
+
+    private void GUIDrawHeader()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("", headerStyle, GUILayout.MinHeight(75), GUILayout.MaxWidth(250));
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+    }
+
+    private void GUIDrawRefreshToggles() {
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.BeginVertical();
+        targetEffect.enableRefresh = EditorGUILayout.BeginToggleGroup("Automatic Refresh", targetEffect.enableRefresh);
+
+        targetEffect.refreshOnFocus = EditorGUILayout.Toggle("Refresh On Window Focus", targetEffect.refreshOnFocus);
+        targetEffect.refreshOnFileChange = EditorGUILayout.Toggle("Refresh On File Change", targetEffect.refreshOnFileChange);
+		targetEffect.refreshAtInterval = EditorGUILayout.Toggle("Refresh At Time Interval", targetEffect.refreshAtInterval);
+
+        if (targetEffect.refreshAtInterval)
+        {
+            targetEffect.refreshInterval = EditorGUILayout.FloatField("Interval (in seconds)", targetEffect.refreshInterval);
+        }
+		
+        EditorGUILayout.EndToggleGroup();
+		GUILayout.EndVertical();
+		GUILayout.EndHorizontal();
+		
+    }
+
+    private void GUIDrawTopShelfButtons()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button(new GUIContent("Refresh"),
+                                GUILayout.MinHeight(30),
+                                GUILayout.MaxWidth(400)
+                                ))
+        {
+            OnButtonRefresh();
+        }
+
+        if (GUILayout.Button(new GUIContent("Open Editor"),
+                                GUILayout.MinHeight(30),
+                                GUILayout.MaxWidth(400)
+                                ))
+        {
+            OnButtonOpenEditor();
+        }
+
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void CheckForUpdates()
+    {
+        Debug.Log("Editor: Checking for updates");
+        Debug.Log(targetEffect._isDirty);
+        if (targetEffect.refreshAtInterval == true) {
+			if (targetEffect._isDirty) {
+                Debug.Log("Editor: Effect is dirty");
+				targetEffect._isDirty = false;
+				targetEffect.Refresh();
+				UpdateOnInterval();
+			}
+		}
+
+		if (_fileSystemWatcher == null) {
+			CreateFileWatcher();
+		}
+
+		if (targetEffect.refreshOnFileChange) {
+				_fileSystemWatcher.EnableRaisingEvents = true;
+		} else {
+				_fileSystemWatcher.EnableRaisingEvents = false;
+		}
+    }
+
+    private void CreateFileWatcher()
+    {
+		_fileSystemWatcher = new FileSystemWatcher();
+		targetEffect.Refresh();
+		_fileSystemWatcher.Path = targetEffect.path;
+
+		/* Watch for changes in LastAccess and LastWrite times, and 
+		the renaming of files or directories. */
+		_fileSystemWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite 
+		| NotifyFilters.FileName | NotifyFilters.DirectoryName;
+		
+		// Only watch xml files.
+		_fileSystemWatcher.Filter = "*.xml";
+
+		// Add event handlers.
+		_fileSystemWatcher.Changed += new FileSystemEventHandler(OnChanged);
+		_fileSystemWatcher.Created += new FileSystemEventHandler(OnChanged);
+		_fileSystemWatcher.Deleted += new FileSystemEventHandler(OnChanged);
+		_fileSystemWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+    }
+
+    private void OnRenamed(object sender, RenamedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnChanged(object sender, FileSystemEventArgs e)
+    {
+        targetEffect.Refresh();
+    }
+
+    IEnumerator UpdateOnInterval() {
+        Debug.Log("Editor: Refreshing in " + targetEffect.refreshInterval + " seconds");
+		yield return new WaitForSeconds(targetEffect.refreshInterval);
+		targetEffect._isDirty = true;
+	}
+
+	private void OnApplicationFocus() {
+		if (targetEffect.refreshOnFocus) {
+			targetEffect.Refresh();
+		}
+	}
+
+    private void OnButtonOpenEditor()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnButtonRefresh() {
+		targetEffect.Refresh();
+	}
+
+	private void GenerateStyles() {
+		headerStyle.alignment = TextAnchor.UpperCenter;
+		headerStyle.normal.background = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Textures/nodefx_logo.png");
+	}
+} 
--- Assets/Scripts/NodeFXEffect.cs
+++ Assets/Scripts/NodeFXEffect.cs
@@ -3,39 +3,252 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using NodeFX;
+using System;
 
-[ExecuteInEditMode]
-[RequireComponent(typeof(ParticleSystem))]
-public class NodeFXEffect : MonoBehaviour {
+namespace NodeFX {
+	public class NodeFXEffect : MonoBehaviour {
 
-	public TextAsset emitterDefinition;
+		public TextAsset effectDefinition;
+		public string path;
 
-	public bool updateOnInterval;
-	public float updateInterval;
-	public bool updateOnFileChanged;
-	public bool updateOnFocus;
-	
-	private bool _isDirty;
-	private XmlDocument doc;
-	private string path;
-	private ParticleSystem _particleSystem;
-	private XMLImporter _importer;
+		public bool enableRefresh;
+		public bool refreshAtInterval;
+		public float refreshInterval = 5.0f;
+		public bool refreshOnFocus;
+		public bool refreshOnFileChange;
+		
+		public bool _isDirty;
+		private XmlDocument doc;
+		private ParticleSystem _particleSystem;
 
-	void OnEnabled() {
-		_importer = new XMLImporter();
-	}
+		void OnEnabled() {
 
-	void OnApplicationFocus() {
-		path = AssetDatabase.GetAssetOrScenePath(emitterDefinition);
-
-		if(!string.IsNullOrEmpty(path) && updateOnFocus) {
-			_importer.LoadXML(path);
 		}
-	}
 
-	IEnumerator checkForUpdates() {
-		yield return new WaitForSeconds(updateInterval);
-		_isDirty = true;
-	}
+        public void Refresh()
+        {
+			Debug.Log("Effect: Refreshing");
+            path = AssetDatabase.GetAssetOrScenePath(effectDefinition);
+			if(!string.IsNullOrEmpty(path)) {
+				LoadXML(path);
+			}
+        }
+
+		public void LoadXML(string path) {
+			Debug.Log("Effect: Loading XML at path " + path);
+			doc = new XmlDocument();
+			doc.Load(path);
+            InstantiateParticleSystem();
+		}
+
+		public int GetIntParam(int emitterIndex, string parameter, int parameterIndex = 0) {
+            string customizedXpath = String.Format("root/emitter[{0}]/attribute[text() = \'{1}\']/value[{2}]", emitterIndex + 1, parameter, parameterIndex + 1);
+			XmlNode node = doc.SelectSingleNode(customizedXpath);
+			
+			return Convert.ToInt32(node.InnerText);
+		}
+
+		public float GetFloatParam(int emitterIndex, string parameter, int parameterIndex = 0) {
+            string customizedXpath = String.Format("root/emitter[{0}]/attribute[text() = \'{1}\']/value[{2}]", emitterIndex + 1, parameter, parameterIndex + 1);
+			XmlNode node = doc.SelectSingleNode(customizedXpath);
+			
+			return Convert.ToSingle(node.InnerText);
+		}
+
+		public string GetStringParam(int emitterIndex, string parameter, int parameterIndex = 0) {
+            string customizedXpath = String.Format("root/emitter[{0}]/attribute[text() = \'{1}\']/value[{2}]", emitterIndex + 1, parameter, parameterIndex + 1);
+			XmlNode node = doc.SelectSingleNode(customizedXpath);
+			return node.InnerText;
+		}
+
+		public Vector4 GetVectorParam(int emitterIndex, string parameter, int parameterIndex = 0) {
+            string customizedXpath = String.Format("root/emitter[{0}]/attribute[text() = \'{1}\']/value[{2}]", emitterIndex + 1, parameter, parameterIndex + 1);
+			XmlNodeList nodes = doc.SelectNodes(customizedXpath);
+
+            Vector4 vector = new Vector4();
+
+            int i = 0;
+            foreach (XmlNode node in nodes) {
+                if (! String.IsNullOrEmpty(node.InnerText)) {
+                    vector[i] = Convert.ToSingle(node.InnerText);
+                } else {
+                    vector[i] = 0;
+                }
+                i++;
+            }
+
+            return vector;
+		}
+
+		public int GetEmitterCount() {
+			string numEmitters = doc.SelectSingleNode("root").Attributes[0].Value;
+			return Convert.ToInt32(numEmitters);
+		}
+
+        private void InstantiateParticleSystem() {
+
+			Transform parent = gameObject.transform;
+
+            DeleteOldParticleSystems();
+
+			for (int i = 0; i < GetEmitterCount(); i++) {
+
+				GameObject Emitter = new GameObject("Emitter" + i);
+				Emitter.transform.parent = parent;
+				ParticleSystem pSystem = Emitter.AddComponent<ParticleSystem>();
+				pSystem.Stop();
+
+				MapMainParameters(pSystem, i);
+				MapEmissionParameters(pSystem, i);
+				MapShapeParameters(pSystem, i);
+				MapVelocityOverLifetimeParameters(pSystem, i);
+				MapLimitVelocityOverLifetimeParameters(pSystem, i);
+				MapInheritVelocityParameters(pSystem, i);
+				MapForceOverLifetimeParameters(pSystem, i);
+				MapColorOverLifetimeParameters(pSystem, i);
+				MapColorBySpeedParameters(pSystem, i);
+				MapSizeOverLifetimeParameters(pSystem, i);
+				MapSizeBySpeedParameters(pSystem, i);
+				MapRotationOverLifetimeParameters(pSystem, i);
+				MapRotationBySpeedParameters(pSystem, i);
+				MapExternalForcesParameters(pSystem, i);
+				MapNoiseParameters(pSystem, i);
+				MapCollisionParameters(pSystem, i);
+				MapTriggerParameters(pSystem, i);
+				MapSubEmitterParameters(pSystem, i);
+				MapTextureSheetAnimationParameters(pSystem, i);
+				MapLightParamters(pSystem, i);
+				MapTrailParameters(pSystem, i);
+				MapCustomDataParameters(pSystem, i);
+				MapRendererParameters(pSystem, i);
+
+				pSystem.Play();
+			}
+		}
+
+        private void DeleteOldParticleSystems()
+        {
+            foreach (ParticleSystem ps in gameObject.GetComponentsInChildren<ParticleSystem>()) {
+                DestroyImmediate(ps.gameObject);
+            }
+        }
+
+        private void MapMainParameters(ParticleSystem pSystem, int i)
+        {
+            Debug.Log("Effect: Mapping parameters: Main");
+            ParticleSystem.MainModule mainModule = pSystem.main;
+			mainModule.duration = GetFloatParam(i, "main_duration");
+            Debug.Log(GetFloatParam(i, "main_duration"));
+        }
+
+        private void MapEmissionParameters(ParticleSystem pSystem, int i)
+        {
+            Debug.Log("Effect: Mapping parameters: Emission");
+        }
+
+        private void MapShapeParameters(ParticleSystem pSystem, int i)
+        {
+            Debug.Log("Effect: Mapping parameters: Shape");
+        }
+
+        private void MapVelocityOverLifetimeParameters(ParticleSystem pSystem, int i)
+        {
+            Debug.Log("Effect: Mapping parameters: VelocityOverLifetime");
+        }
+
+        private void MapLimitVelocityOverLifetimeParameters(ParticleSystem pSystem, int i)
+        {
+            Debug.Log("Effect: Mapping parameters: LimitVelocityOverLifetime");
+        }
+
+        private void MapInheritVelocityParameters(ParticleSystem pSystem, int i)
+        {
+            Debug.Log("Effect: Mapping parameters: InheritVelocity");
+        }
+
+        private void MapForceOverLifetimeParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapColorOverLifetimeParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapColorBySpeedParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapSizeOverLifetimeParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapSizeBySpeedParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapRotationOverLifetimeParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapRotationBySpeedParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapExternalForcesParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapNoiseParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapCollisionParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapTriggerParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapSubEmitterParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapTextureSheetAnimationParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapLightParamters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapTrailParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapCustomDataParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapRendererParameters(ParticleSystem pSystem, int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
