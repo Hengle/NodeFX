fe22903d6e39ab370f6af8dab33f822a6269d873
cjudkins <mail@chrisjudkins.com>
2018-03-13 14:47:28 -0400

    Separated Houdini and Unity files into their own separate subfolders

--- Houdini/scripts/123.cmd
+++
@@ -1,2 +0,0 @@
-set HFS=C:\Program Files\Side Effects Software\Houdini 16.5.323
-set PATH=$HFS$\bin;$PATH$
--- Houdini/scripts/Untitled-1.py
+++
@@ -1,5 +0,0 @@
-node = hou.pwd()
-definition = node.type().definition()
-path = node.parm("HDApath").get()
-
-definition.save(path, node)
